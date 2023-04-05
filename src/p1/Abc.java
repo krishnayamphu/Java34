5eb282c33313d2b89c912e6aa43df916ae488dcd
KrishnaYamphu <krishnayamphu@gmail.com>
2023-04-05 15:47:47 +0545

    package

---
+++ src/p1/Abc.java
@@ -0,0 +1,11 @@
+package p1;
+
+import inheritance.*;
+
+public class Abc {
+    public static void main(String[] args) {
+        Dog dog=new Dog();
+        dog.color="Red";
+        System.out.println(dog.color);
+    }
+}
---
+++ src/p1/EncapsulationDemo.java
@@ -0,0 +1,10 @@
+package p1;
+
+public class EncapsulationDemo {
+    public static void main(String[] args) {
+        Employee e=new Employee();
+        e.setName("Ram");
+
+        System.out.println(e.getName());
+    }
+}
